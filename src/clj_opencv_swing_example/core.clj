67ac7c93dc1c1c064b0c6d8c7d5d0deba70253ec
asuki <asukiaaa@gmail.com>
2018-01-23 21:21:27 +0900

    Create buffer before loop to prevent OutOfMemoryError

--- src/clj_opencv_swing_example/core.clj
+++ src/clj_opencv_swing_example/core.clj
@@ -6,42 +6,31 @@
            [org.opencv.imgproc Imgproc])
   (:require [clojure.core.async :as async]))
 
-(def lena (Imgcodecs/imread "img/lena.jpg"))
-(def frame (new JFrame))
+(def color-lena-mat (Imgcodecs/imread "img/lena.jpg"))
+(def j-frame (new JFrame))
 
-(defn mat->buffered-image [mat]
+(defn config-j-frame [j-frame & {:keys [title set-visible close-operation width height]}]
+  (when title
+    (.setTitle j-frame title))
+  (when-not (nil? set-visible)
+    (.setVisible j-frame set-visible))
+  (when close-operation
+    (.setDefaultCloseOperation j-frame close-operation))
+  (when (and width height)
+    (.setSize j-frame width height)))
+
+(defn create-buffered-image [mat]
   (let [gray? (= (.channels mat) 1)
         type (if gray?
                BufferedImage/TYPE_BYTE_GRAY
-               BufferedImage/TYPE_3BYTE_BGR)
-        image (new BufferedImage (.width mat) (.height mat) type)
-        target-pixels (-> image
-                          (.getRaster)
-                          (.getDataBuffer)
-                          (.getData))
-        raster (.getRaster image)
-        buffer (byte-array (* (.width mat) (.height mat) (if gray? 1 3)))]
-    (prn :gray gray?)
-    (.get mat 0 0 buffer)
-    (System/arraycopy buffer 0 target-pixels 0 (alength buffer))
-    image))
+               BufferedImage/TYPE_3BYTE_BGR)]
+    (new BufferedImage (.width mat) (.height mat) type)))
 
-(defn show-mat [frame mat & {:keys [title set-visible close-operation]}]
-  (when title
-    (.setTitle frame title))
-  (when-not (nil? set-visible)
-    (.setVisible frame set-visible))
-  (-> frame
-      (.getContentPane)
-      (.removeAll))
-  (.setSize frame (.width mat) (.height mat))
-  (.add frame (->> (mat->buffered-image mat)
-                   (new ImageIcon)
-                   (new JLabel)))
-  (.revalidate frame)
-  (.repaint frame)
-  (when close-operation
-    (.setDefaultCloseOperation frame close-operation)))
+(defn update-buffered-image [buffered-image mat]
+  (.get mat 0 0 (-> buffered-image
+                    (.getRaster)
+                    (.getDataBuffer)
+                    (.getData))))
 
 (defn color->gray [mat]
   (let [gray-mat (new Mat)]
@@ -49,13 +38,28 @@
     gray-mat))
 
 (defn -main []
-  (show-mat frame lena
-            :title "opencv mat on swing"
-            :set-visible true
-            :close-operation WindowConstants/EXIT_ON_CLOSE)
-  (async/go-loop [seconds 1]
-    (async/<! (async/timeout 1000))
-    (show-mat frame (if (even? seconds)
-                      lena
-                      (color->gray lena)))
-    (recur (inc seconds))))
+  (config-j-frame j-frame
+                  :title "opencv mat on swing"
+                  :set-visible true
+                  :close-operation WindowConstants/EXIT_ON_CLOSE
+                  :width (.width color-lena-mat) :height (.height color-lena-mat))
+  (let [color-buffered-image (create-buffered-image color-lena-mat)
+        _ (update-buffered-image color-buffered-image color-lena-mat)
+        color-panel (->> color-buffered-image
+                        (new ImageIcon)
+                        (new JLabel))
+        gray-lena-mat (color->gray color-lena-mat)
+        gray-buffered-image (create-buffered-image gray-lena-mat)
+        _ (update-buffered-image gray-buffered-image gray-lena-mat)
+        gray-panel (->> gray-buffered-image
+                            (new ImageIcon)
+                            (new JLabel))]
+    (async/go-loop [seconds 1]
+      (async/<! (async/timeout 1000))
+      (-> j-frame
+          .getContentPane
+          .removeAll)
+      (.add j-frame (if (even? seconds) color-panel gray-panel))
+      (.revalidate j-frame)
+      (.repaint j-frame)
+      (recur (inc seconds)))))
